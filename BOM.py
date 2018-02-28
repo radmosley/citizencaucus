76e26f7c02f49664c7cbfbebc3ceca3579946255
RMosley <ronnie.j.mosley@gmail.com>
2018-02-27 21:15:29 -0800

    AWS setup updated

--- BOM.py
+++
@@ -1,6 +0,0 @@
-with open('database.json', 'rb') as f:
-    contents = f.read()
-    contents = contents.decode("utf-16")
-
-with open('database-B.json', 'w', encoding='utf-8') as f:
-    f.write(contents)
