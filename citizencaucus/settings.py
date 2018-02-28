a0c682b5e22160c841d2e1d29f4024163ef15365
RMosley <ronnie.j.mosley@gmail.com>
2018-02-28 11:37:02 -0800

    deployment

--- citizencaucus/settings.py
+++ citizencaucus/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'citizencaucus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'db.sqlite3')],
+        'DIRS': [os.path.join(BASE_DIR, 'pages', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
