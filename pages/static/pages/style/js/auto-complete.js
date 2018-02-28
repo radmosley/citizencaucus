53309f29e9e5ffe4102f3a57cb3b2fb563906748
RMosley <ronnie.j.mosley@gmail.com>
2018-02-28 11:58:52 -0800

    deployment

--- pages/static/pages/style/js/auto-complete.js
+++ pages/static/pages/style/js/auto-complete.js
@@ -20,7 +20,7 @@ $('#search').keyup(function(){
     }
 });
 
-$(document).ready(function(){
+$(document).ready(function(event){
 //open dropdown on search bar focus
     $("#searchbox").focus(function(){
         $('#search-results').show();
@@ -29,7 +29,7 @@ $(document).ready(function(){
 });
 
 //close dropdown on click of window other than the search bar or search results and clear input
-$(window).click(function(){
+$(window).click(function(event){
     if ($("#search > input[type=text]").length > 0){
         $("#searchbox").val('');
         $("#search-results").hide();
